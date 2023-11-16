aabe2b7cd7366d2784f3252262e16fb03460ef37
Mohammad Mansour <mohammed.i02@hotmail.com>
2023-11-16 16:25:04 +0200

    Initiate project

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/note-keeping-app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+const notesRouter = require("./routes/notes");
+app.use("/notes", notesRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ server/routes/notes.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const Note = require("../models/Note");
+
+router.get("/", async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.json(notes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const note = new Note({
+    title: req.body.title,
+    content: req.body.content,
+    createdAt: req.body.createdAt || new Date(),
+  });
+  try {
+    const newNote = await note.save();
+    res.status(201).json(newNote);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+
+    if (!deletedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res.status(204).json({ message: "Note deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting note" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      {
+        title: req.body.title,
+        content: req.body.content,
+        createdAt: req.body.createdAt,
+      },
+      { new: true }
+    );
+
+    if (!updatedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res.json(updatedNote);
+  } catch (err) {
+    res.status(500).json({ message: "Error updating note" });
+  }
+});
+
+module.exports = router;
